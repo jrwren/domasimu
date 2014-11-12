050ef1b48a1586d4fe9fa5cb9cac8508c404bbc9
Jay Wren <jrwren@xmtp.net>
2014-11-11 21:11:23 -0500

    add license

--- cmd/domasimu/main.go
+++ cmd/domasimu/main.go
@@ -1,3 +1,8 @@
+// Copyright © 2014 Jay R. Wren <jrwren@xmtp.net>.
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
+
 package main
 
 import (
